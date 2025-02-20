9c1c01a723706adecd0f7f51243cc8d92935faf8
João RS <111208120+JOAORSS@users.noreply.github.com>
2025-02-20 06:22:10 +0000

    finally fixed all in usuario

--- src/components/perfilPagina/perfil/usuario/index.tsx
+++ src/components/perfilPagina/perfil/usuario/index.tsx
@@ -8,20 +8,24 @@ export default async function Usuario() {
 
     const session = await auth();
 
+    const image = session?.user?.image;
+    const name = session?.user?.name;
+    const email = session?.user?.email;
+
     return(
         <div>
             <h3 className={styles.infoBoxLabel}>Usuario</h3>
             <div className={styles.infoBox}>
                 <Image 
                     className={styles.imagemUsuario}
-                    src={session?.user?.image}
+                    src={image!}
                     alt="imagem do usuario"
-                    width={100}
-                    height={100}
+                    width={200}
+                    height={200}
                 />
                 <div className={styles.perfilInfo}>
-                    <h3 className={styles.nome}>João Vitor Raneke dos Santos</h3>
-                    <p className={styles.email}>joaors987654321@gmail.com</p>
+                    <h3 className={styles.nome}>{name!}</h3>
+                    <p className={styles.email}>{email!}</p>
                 </div>
             </div>
         </div>
