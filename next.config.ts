a8e265bde1db690f150a4e55715f65d49523702e
JoaoRS <joaors987654321@gmail.com>
2025-02-21 14:22:39 -0300

    adidionado dominios do facebok para os Images

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,19 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'lh3.googleusercontent.com'
       },
+      {
+        protocol: "https",
+        hostname: "platform-lookaside.fbsbx.com",
+        port: "",
+        pathname: "/**", // Permite todas as rotas desse domínio
+      },
+      // Instagram (caso o perfil esteja vinculado ao Instagram)
+      {
+        protocol: "https",
+        hostname: "scontent.cdninstagram.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
